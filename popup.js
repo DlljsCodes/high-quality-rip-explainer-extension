2facc72ee86b93b0866b1bb89a063bded9f9cacc
Lexi Stevens <lexi@stevensfamily.co.uk>
2021-04-16 17:47:52 +0100

    Add get YouTube video data function

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ const link = 'https://siivagunner.fandom.com/wiki/';
 
 var tabUrl;
 var videoTitle;
+var error = false;
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var tab = tabs[0];
@@ -10,15 +11,29 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
     console.log("Current Tab URL: " + url);
     tabUrl = url;
-    if checkIfYouTubeUrl(tabUrl) {
-		// 
+	youTubeCheck = checkIfYouTubeUrl(tabUrl);
+    if (youTubeCheck) {
+		getYouTubeVideoData(tabUrl); // Temporary call
 	} else {
 		// Disable button and show appropriate info
 	}
 });
 
+function getYouTubeVideoData(videoUrl) {
+	const dataUrlPre = "https://youtube.com/oembed?url=";
+	const dataUrlSuf = "&format=json";
+	var dataUrl = dataUrlPre + videoUrl + dataUrlSuf;
+	console.log("OEmbed URL: " + dataUrl);
+	fetch(dataUrl)
+		.then(result => result.json())
+		.then((out) => {
+		console.log("Success! Output: ", out);
+		return out;
+	}).catch(err => {console.error(err)});
+}
+
 function checkIfYouTubeUrl(url) {
-	if (url.slice(0, 23) = "https://www.youtube.com") {
+	if (url.slice(0, 23) == "https://www.youtube.com") {
 		console.log("Is a YouTube URL");
 		return true;
 	} else {
